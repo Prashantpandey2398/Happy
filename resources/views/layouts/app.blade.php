e6b69f57cb7a5d8da28fb9e40a5b66b08476048a
Kasparas Zutautas <kz@Kasparass-MacBook-Pro.local>
2017-11-18 17:58:27 +0000

    Fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,11 @@
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
+                    @if(Auth::user)
                     <ul class="nav navbar-nav">
                         <li><a href="{{ route('articles.index') }}">Articles</a></li>
                     </ul>
+                    @endif
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
