a03aa175dc868f5cfc6ef5f0c5bc62abdc43b063
Saurav KC <sauravkc.np@gmail.com>
2017-12-31 22:39:36 +0545

    heighe i

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -142,7 +142,7 @@
                     var height = closest_element.height()+50;
                     if($(this).attr('aria-expanded') === 'true'){
                         console.log('notclick');
-                        $('.note-toolbar').height(55px);
+                        $('.note-toolbar').height(55);
                     }
                     else {
                         $('.note-toolbar').height(height + 25);
@@ -152,7 +152,7 @@
             });
             $(".dropdown-toggle").blur(function () {
                 if($(this).hasClass("note-btn")) {
-                    $('.note-toolbar').height(55px);
+                    $('.note-toolbar').height(55);
 
                 }
             });
