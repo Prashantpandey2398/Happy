f824663f2fb583f9abf74dfe9c5ffc7a13122534
Saurav KC <sauravkc.np@gmail.com>
2018-01-06 17:33:11 +0545

    route dinied for home and index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,19 @@ class HomeController extends Controller
      */
     public function index()
     {
+        if(Auth::check())
+        {
+            return redirect()->route('articles.index');
+        }
         return view('home');
+    }
+
+    public function main()
+    {
+        if(Auth::check())
+        {
+           return redirect()->route('articles.index');
+        }
+        return view('welcome');
     }
 }
